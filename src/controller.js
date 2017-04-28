594ccda7ce6620c70112933ad5ac6fee976fa398
Patrick Smith <patrick@promptworks.com>
2017-04-28 10:37:24 -0400

    Add redis backed storage to botkit bot

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,13 @@
 import { sparkbot } from 'botkit'
+import redisStorage from 'botkit-storage-redis'
 
 const controller = sparkbot({
   debug: true,
   log: true,
   public_address: process.env.PUBLIC_ADDRESS || 'https://example.com',
   ciscospark_access_token: process.env.ACCESS_TOKEN || 'token',
-  studio_token: process.env.STUDIO_TOKEN
+  studio_token: process.env.STUDIO_TOKEN,
+  storage: redisStorage(process.env.REDIS_URL)
 })
 
 export default controller
