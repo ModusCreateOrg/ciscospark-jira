6b6f554dd8eb739e927f0758316c807df4e69c26
Patrick Smith <patrick@promptworks.com>
2017-05-01 11:10:58 -0400

    Update help message with watch commands

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -5,19 +5,21 @@ import webhooks from './webhooks'
 const displayHelp = (bot, message) => bot.reply(message, `
   Here are some of the things I can do:
   - **list open tickets** — list open issues assigned to you or someone else. For \
-     example, to list your own issues, try \`list my open issues\`. To list issues \
-     for someone else try \`list open issues for George\`.
+    example, to list your own issues, try \`list my open issues\`. To list issues \
+    for someone else try \`list open issues for George\`.
   - **create tickets** — I can create a new task, story, or bug for you. You \
-     must specify the project, type of ticket and summary. For example: \
-     \`create new TEST task "Add more features"\`.
+    must specify the project, type of ticket and summary. For example: \
+    \`create new TEST task "Add more features"\`.
   - **ticket status** — I can find the status of an existing ticket. You can \
-     ask, for example, \`what is the status of TEST-12?\`.
+    ask, for example, \`what is the status of TEST-12?\`.
   - **comment on a ticket** - To comment on an issue, you can tell me which ticket \
-     and your comment: \`comment on TEST-12 "These features are important"\`
+    and your comment: \`comment on TEST-12 "These features are important"\`
+  - **watch a ticket** - I can notify you of changes to a particular ticket. \
+    To receive notifications about "TEST-12", for example, you can say \
+    \`start watching TEST-12\`. To stop receiving notifications, you can tell \
+    me to stop watching a ticket: \`stop watching TEST-12\`. To see a list of \
+    tickets I'm watching you can use: \`list watched tickets\`.
   - **help** — display this message
-
-  I'll also post notifications to ${process.env.JIRA_WEBHOOK_ROOM} \
-  when issues are created, assigned, or their status changes.
 `)
 
 export const handleJoin = (bot, message) =>
