6afc869ae86fe7974d0b651d91806fa6f8cbffd9
Patrick Smith <patrick@promptworks.com>
2017-05-01 09:51:38 -0400

    Add `promisify-node` to promisify redis storage

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,17 @@
 import { sparkbot } from 'botkit'
 import redisStorage from 'botkit-storage-redis'
+import promisify, { callbacks } from 'promisify-node'
+import args from 'promisify-node/utils/args'
+
+// Promisify determines if a function can be turned into a promise
+// using the name of the last argument to the method. If it's named like a
+// callback, assume its a callback. This works for everything except for
+// the `all` method of `botkit-storage-redis`, which has a signature of
+// `all(cb, options)`. To make this promisify-able, we take advantage of
+// being able to pass in a "test" function and check whether the first argument
+// is callback-like.
+const promisifyHack = (exports, exportsName, parentKeyName) =>
+  callbacks.indexOf(args(exports)[0]) > -1
 
 const controller = sparkbot({
   debug: true,
@@ -7,7 +19,10 @@ const controller = sparkbot({
   public_address: process.env.PUBLIC_ADDRESS || 'https://example.com',
   ciscospark_access_token: process.env.ACCESS_TOKEN || 'token',
   studio_token: process.env.STUDIO_TOKEN,
-  storage: redisStorage(process.env.REDIS_URL)
+  storage: promisify(redisStorage({
+    url: process.env.REDIS_URL,
+    methods: ['tickets']
+  }), promisifyHack)
 })
 
 export default controller
